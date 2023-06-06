1fb3c6ad9c6ec1fb7e07fe452e1e92e134570696
diasjoaoluiz <joao_dacosta@live.com>
2023-06-06 18:22:18 -0300

    alteracoes no responsivel e adicao de secao de experiencia

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -6,14 +6,15 @@ const phrases = [
   let currentPhraseIndex = 0;
   let currentPhrase = phrases[currentPhraseIndex];
   let charIndex = 0;
+  let tagText = document.getElementById("description-text");
   
   function typeWriter() {
     if (charIndex < currentPhrase.length) {
-      document.getElementById("description-text").textContent += currentPhrase.charAt(charIndex);
+      tagText.textContent += currentPhrase.charAt(charIndex);
       charIndex++;
-      setTimeout(typeWriter, 150); // velocidade de digitação (ajuste conforme necessário)
+      setTimeout(typeWriter, 150); // velocidade de digitação 
     } else {
-      setTimeout(changePhrase, 4000); // tempo para trocar de frase (5 segundos)
+      setTimeout(changePhrase, 4000); // tempo para trocar de frase
     }
   }
   
@@ -21,9 +22,51 @@ const phrases = [
     charIndex = 0;
     currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
     currentPhrase = phrases[currentPhraseIndex];
-    document.getElementById("description-text").textContent = "";
+    tagText.textContent = "";
     typeWriter();
   }
   
   typeWriter();
-  
+
+
+const imagem = document.getElementById("scrollImage");
+imagem.addEventListener('click', function() {
+  const divContente = document.getElementById('about');
+  divContente.scrollIntoView({ behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', function() {
+  const arrowImage = document.getElementById('arrowImage');
+  const scrollPosition = window.scrollY;
+
+  // Define a quantidade de rolagem que ativa a exibição da imagem
+  const scrollThreshold = 200;
+
+  if (scrollPosition > scrollThreshold) {
+    arrowImage.classList.remove('hidden');
+  } else {
+    arrowImage.classList.add('hidden');
+  }
+});
+
+
+window.addEventListener('scroll', function() {
+  const arrowImage = document.getElementById('arrowImage');
+  const scrollPosition = window.scrollY;
+  const scrollThreshold = 200;
+
+  if (scrollPosition > scrollThreshold) {
+    arrowImage.classList.remove('hidden');
+  } else {
+    arrowImage.classList.add('hidden');
+  }
+});
+
+const arrowImage = document.getElementById('arrowImage');
+arrowImage.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
